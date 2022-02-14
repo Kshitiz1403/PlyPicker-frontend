5f7066b801b31d6f0d9a164b64af407e91c2bf2e
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-02-14 14:26:12 +0530

    implement basic logic for mega menu

--- src/components/MegaMenu.js
+++ src/components/MegaMenu.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 const MegaMenu = () => {
-    const [showNavItem, setShowNavItem] = useState(true)
-    const [activeNavItem, setActiveNavItem] = useState('Wood')
 
     const data = {
         "Wood":
@@ -17,11 +15,43 @@ const MegaMenu = () => {
         }
     }
 
+    // Controls the visibility of the categories menu
+    // By default, the menu is not shown
+    const [showNavItem, setShowNavItem] = useState(false)
+
+    // Stores the Navbar title. For eg. Wood. 
+    // Responsible for controlling the categories for the menu
+    // Defaults to the first item in the data object
+    const [activeNavItem, setActiveNavItem] = useState(Object.keys(data)[0])    
 
     const navItems = Object.keys(data)
 
     const NavItem = (props) => {
         const [isShowed, setIsShowed] = useState(false)
+
+        // function useOutsideAlerter(ref) {
+        //     useEffect(() => {
+        //         /**
+        //          * Alert if clicked on outside of element
+        //          */
+        //         function handleClickOutside(event) {
+        //             if (ref.current && !ref.current.contains(event.target)) {
+        //                 alert("You clicked outside of me!");
+        //             }
+        //         }
+
+        //         // Bind the event listener
+        //         document.addEventListener("mousedown", handleClickOutside);
+        //         return () => {
+        //             // Unbind the event listener on clean up
+        //             document.removeEventListener("mousedown", handleClickOutside);
+        //         };
+        //     }, [ref]);
+        // }
+
+        // const wrapperRef = useRef(null);
+        // useOutsideAlerter(wrapperRef);
+
 
         const mouseOverAction = () => {
             setShowNavItem(true)
@@ -31,6 +61,12 @@ const MegaMenu = () => {
         const mouseLeaveAction = () => {
             setShowNavItem(false)
             setIsShowed(false)
+            setActiveNavItem(props.title)
+        }
+
+        const mouseClickAction = () => {
+            setShowNavItem(!showNavItem)
+            setIsShowed(!isShowed)
             setActiveNavItem(props.title)
         }
 
@@ -49,62 +85,95 @@ const MegaMenu = () => {
 
         return (
             <div>
-                <div style={{ borderBottomWidth: isShowed ? 2 : 0, ...navItemStyle }} onMouseOver={mouseOverAction} onMouseLeave={mouseLeaveAction}>
+                <div style={{ borderBottomWidth: isShowed ? 2 : 0, ...navItemStyle }}
+                    //  onMouseOver={mouseOverAction}
+                    //   onMouseLeave={mouseLeaveAction}
+                    onClick={mouseClickAction}
+                >
                     {props.title}
-                    <MiniMenu title={props.title} />
                 </div>
             </div>
-
         )
     }
 
     const MiniMenu = (props) => {
-        const [isActive, setIsActive] = useState(false)
-
+        // Array of keys for the selected category. 
+        // For eg. => Selected catgeory = wood, miniMenuData = [plywood, blackwood]
         let miniMenuData = Object.keys(data[props.title])
-        const [activeView, setActiveView] = useState("plywood")
-        // console.log(miniMenuData.map(item=> data[props.title][item]))
+
+        // The selected sub category for the selected category
+        // For eg. plywood
+        const [activeView, setActiveView] = useState(Object.keys(data[activeNavItem])[0])
+
+        const MiniMenuStyle = {
+            containerStyle: {
+                height: 250,
+                backgroundColor: 'blue',
+                top: 50,
+                position: 'absolute',
+                display: showNavItem ? 'flex' : 'none',
+            }
+        }
+
+        const SubMenu = () => {
+            const subCategoryStyle = {
+                containerStyle: {
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minWidth: "25vmin"
+                },
+                itemStyle: {
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    backgroundColor: 'red',
+                    cursor: 'pointer'
+                }
+            }
+
+            return <div style={subCategoryStyle.containerStyle}>
+                {miniMenuData.map(item => <div key={item} style={subCategoryStyle.itemStyle} onClick={() => setActiveView(item)}>
+                    <div>{item}</div>
+                    <div>{activeView == item ? ">" : null}</div>
+                </div>
+                )}
+            </div>
+        }
+
+
+        const NestedMenu = () => {
+            const nestedCategoryStyle = {
+                container: {
+                    display: 'flex', flexDirection: 'column', backgroundColor: 'green', minWidth: "25vmin"
+                }
+            }
+            let title = data[props.title][activeView]
+            return (
+                <div style={nestedCategoryStyle.container}>
+                    {title.map(item => <div style={{ cursor: 'pointer' }} key={item}>{item}</div>)}
+                </div>
+            )
+        }
+
         return (
-            <div style={{ display: showNavItem ? 'flex' : 'none', height: 250, width: '60%', backgroundColor: 'blue', position: 'absolute', top: 50 }}>
-                <div style={{ display: 'flex', flexDirection: 'column', width: '25%' }}>
-
-                    {miniMenuData.map(item => <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'red', cursor: 'pointer' }}>
-                        <div>{item}</div>
-                        <div>{">"}</div>
-                    </div>
-                    )}
-
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'column', width: '25%' }}>
-                    <SubMenu title={data[props.title][activeView]} />
-                </div>
+            <div style={MiniMenuStyle.containerStyle}>
+                <SubMenu />
+                <NestedMenu />
             </div>
         )
     }
 
-    const SubMenu = (props) => {
-        let arr = props.title
-        console.log(arr)
-        return (<>
-        {arr}
-
-            {/* {arr.forEach(element => {
-            <div>{element}</div>
-        })} */}
-        </>
-        )
+    const navBarStyle = {
+        container: {
+            height: 50, display: 'flex', flexDirection: 'row', alignItems: 'center', borderStyle: 'solid', borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, borderBottomWidth: 0.5, borderColor: 'black'
+        }
     }
 
     return (
         <div>
-            <div style={{ width: '100%', height: 50, display: 'flex', flexDirection: 'row', alignItems: 'center', borderStyle: 'solid', borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, borderBottomWidth: 0.5, borderColor: 'black' }}
-            >
-                {navItems.map(item => {
-                    // console.log(Object.keys(data[item]))
-                    return (<NavItem title={item} />)
+            <div style={navBarStyle.container}>
+                {navItems.map(item => <NavItem key={item} title={item} />)
                 }
-
-                )}
             </div>
             <MiniMenu title={activeNavItem} />
         </div>
